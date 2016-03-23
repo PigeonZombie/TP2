a8aedb7bef332c768cfdb42b96316f69429bf75c
vivas5 <felixre-94@hotmail.com>
2016-03-23 12:54:27 -0400

    COmmentaire minuteur

--- app/src/main/java/ca/csf/tp2/Modele/Minuteur.java
+++ app/src/main/java/ca/csf/tp2/Modele/Minuteur.java
@@ -9,6 +9,12 @@ import ca.csf.tp2.Modele.Portail.ObservateurMinuteur;
 /**
  * Created by Utilisateur on 2016-03-18.
  */
+
+/**
+ * Classe pour faire le minuteur de la partie, contient deux timer Java un pour la partie , et un pour le joueur en cour
+ *
+ *@author Felix
+ */
 public class Minuteur implements InterfaceMinuteur {
 
     public static final int DUREE_PARTIE = 1200000;
@@ -18,15 +24,24 @@ public class Minuteur implements InterfaceMinuteur {
     private Timer minuteurEtudiant = null;
     private long tempsPourEtudiant = 60000;
 
+    /**
+     * Constructeur de la classe minuteur, crée le minuteur globale ainci que la première instance du minuteur de joueur
+     * @param _observateurMinuteur
+     */
     public Minuteur(ObservateurMinuteur _observateurMinuteur){
         this.observateurMinuteur = _observateurMinuteur;
         minuteurPartie = new Timer();
         creerMinuteurEtudiant();
         minuteurPartie.schedule (initialiserTacheMinuteurPartie(), DUREE_PARTIE);
-
     }
 
 
+    /**
+     * Quand un étudiant est trouvé, crée un nouveau timer pour un autre étudiant et retourne le temps restant pour
+     * trouver l'ancien étudiant pour servire de pointage
+     *
+     * @return le temps restant pour trovuer l'ancien étudiant
+     */
     @Override
     public long quandEtudiantTrouvee() {
         long aRetourner = tempsPourEtudiant;
@@ -37,6 +52,10 @@ public class Minuteur implements InterfaceMinuteur {
     }
 
 
+    /**
+     * Crée la tâche du minuteur globale
+     * @return la tâche du minuteur globale
+     */
     private TimerTask initialiserTacheMinuteurPartie(){
         TimerTask tacheMinuteurPartie = new TimerTask() {
             @Override
@@ -47,6 +66,10 @@ public class Minuteur implements InterfaceMinuteur {
         return tacheMinuteurPartie;
     }
 
+    /**
+     * Crée la tâche du minuteur du joueur servant à décrémenter le temps restant
+     * @return la tâche du minuteur du joueur servant à décrémenter le temps restant
+     */
     private TimerTask initialiserTacheMinuteurJoueurScore(){
         TimerTask tacheMinuteurJoueurScore = new TimerTask() {
             @Override
@@ -58,6 +81,10 @@ public class Minuteur implements InterfaceMinuteur {
         return tacheMinuteurJoueurScore;
     }
 
+    /**
+     * Crée la tâche du minuteur du joueur servant à dire quand la fin du temps pour ce joueur
+     * @return la tâche du minuteur du joueur servant à dire quand la fin du temps pour ce joueur
+     */
     private TimerTask initialiserTacheMinuteurJoueurTempsTotal(){
         TimerTask tacheMinuteurJoueurTempsTotal = new TimerTask() {
             @Override
@@ -68,7 +95,9 @@ public class Minuteur implements InterfaceMinuteur {
         return tacheMinuteurJoueurTempsTotal;
     }
 
-
+    /**
+     * Crée le minuteur pour le joueur et lui assigne ses tâches.
+     */
     private void creerMinuteurEtudiant(){
         minuteurEtudiant = new Timer();
         tempsPourEtudiant = 60000;
